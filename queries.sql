8deab3b04925d8c7843ce2cb1577bc3932208c5e
vy3191 <venkyafhs@gmail.com>
2020-03-05 21:32:09 -0500

    Completed Queries#1

--- queries.sql
+++ queries.sql
@@ -1,6 +1,10 @@
 -- Multi-Table Query Practice
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
+SELECT C.CategoryName, P.ProductName
+FROM "Category" AS C
+JOIN "Product" AS P
+ON C.Id = P.CategoryId
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 
