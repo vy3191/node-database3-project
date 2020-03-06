881381e316c896c1517d37b49909b37e9ae6818b
vy3191 <venkyafhs@gmail.com>
2020-03-05 22:20:59 -0500

    Completed Queries#2

--- queries.sql
+++ queries.sql
@@ -7,7 +7,11 @@ JOIN "Product" AS P
 ON C.Id = P.CategoryId
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
-
+SELECT O."Id", S."CompanyName"
+FROM "Order" AS O
+JOIN "Shipper" AS S
+ON S."Id" = O."ShipVia"
+WHERE "OrderDate" < "2012-08-09";
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
